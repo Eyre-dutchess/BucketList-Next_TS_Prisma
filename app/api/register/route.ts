ce205e94c4762554aeca1df2a454769e28dd22e4
Eyre Xi <109657355+Eyre-dutchess@users.noreply.github.com>
2025-06-10 16:55:41 +0800

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 import bcryptjs from "bcryptjs"
 
-export const POST =async (request:Request) =>{
+export const POST =async (request:Request): Promise<NextResponse>  =>{
     const resp = await request.json()
     const {email, name, password} = resp;
     const hashedPassword = await bcryptjs.hash(password, 12)
